bc6bdc0829924a91cdb08a3c9d8de7c350d3d082
jovimassimiliano <massimiliano.giovanni@yahoo.com>
2017-04-12 18:04:58 +0700

    settings for heroku

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var express = require("express");
 //Create app
 
 var app = express();
+const port = process.env.PORT || 3000;
 
+app.use(function(req,res,next){
+  if(req.headers["x-forwarded-proto"] === "http"){
+    next();
+  }else {
+    res.redirect("http://" + req.hostname + req.url);
+  }
+});
 app.use(express.static("public"));
 
-app.listen(3000,function(){
-  console.log("Port 3000 has been up");
+app.listen(port,function(){
+  console.log("Port "+ port +" has been up");
 })

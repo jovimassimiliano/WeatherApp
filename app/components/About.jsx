736e8b5fd30f55e97cb1e0b6f85fa86656529a0d
jovimassimiliano <massimiliano.giovanni@yahoo.com>
2017-04-16 17:23:26 +0700

    install scss

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var React = require("react");
 var About = (props) => {
   return(
     <div className>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>
         This app created using React.
       </p>
